ed412be9568ff125ea9c230994af4c0e46801703
Drew Bowman <bowmande@gmail.com>
2010-12-04 11:18:19 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebooker2Sample::Application.config.secret_token = '437310b361e766d2357434aba57af2ba83799d8bae381bc921fd4ee9ebff816d8614a5896349e6f4ea98437b7a82ea83ead56f2ddb3e2c920f796913ae89102b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Facebooker2Sample::Application.config.session_store :cookie_store, :key => '_Facebooker2Sample_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Facebooker2Sample::Application.config.session_store :active_record_store
